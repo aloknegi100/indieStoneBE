1fe522f34862ef62f0ef199c9220876120ec4f43
Alok Negi <aloknegi@Aloks-MacBook-Air.local>
2023-08-09 23:52:30 +0530

    added products

--- controller/user.js
+++ controller/user.js
@@ -1,108 +1,110 @@
 const User = require("../models/user");
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken');
-const ProductSchema = require('../models/product.js')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const ProductSchema = require("../models/product.js");
 
-exports.login = async function(req,res){    
+exports.login = async function (req, res) {
+  try {
+    const email = req.body.email;
+    const password = req.body.psd;
 
-    try {
-        const email = req.body.email;
-        const password = req.body.psd;
-    
-        // Find the user by username
-        const user = await User.findOne({ email });
-        if (!user) {
-          return res.json({ success:false, message: 'Invalid username or password' });
-        }
-    
-        // Compare the password
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-        if (!isPasswordValid) {
-          return res.json({success:false, message: 'Invalid username or password' });
-        }
-    
-        // Create and sign a JWT
-        const token = jwt.sign({ userId: user._id }, 'indiestonesecret', { expiresIn: '6h' });
-    
-        res.json({ success:true,token ,message:"login success"});
-      } catch (error) {
-        console.error(error);
-        res.json({ success:false,message: 'Server error' });
-      }
-    
+    // Find the user by username
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.json({
+        success: false,
+        message: "Invalid username or password",
+      });
+    }
 
+    // Compare the password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.json({
+        success: false,
+        message: "Invalid username or password",
+      });
+    }
 
-}
-exports.register = async function(req,res){ 
-    
-    try{
+    // Create and sign a JWT
+    const token = jwt.sign({ userId: user._id }, "indiestonesecret", {
+      expiresIn: "6h",
+    });
 
-    
-
+    res.json({ success: true, token, message: "login success" });
+  } catch (error) {
+    console.error(error);
+    res.json({ success: false, message: "Server error" });
+  }
+};
+exports.register = async function (req, res) {
+  try {
     const email = req.body.newEmail;
     const password = req.body.newpsd;
 
-
-    let user =await User.findOne({email})
-    console.log("user ",user)
-    if(user){
-       return res.json({
-            success:false,
-            message:"User with this email already exist"
-        })
+    let user = await User.findOne({ email });
+    console.log("user ", user);
+    if (user) {
+      return res.json({
+        success: false,
+        message: "User with this email already exist",
+      });
     }
 
-    const hashpsd = await bcrypt.hash(password,10)
+    const hashpsd = await bcrypt.hash(password, 10);
 
-    const newUser = new User({email,password:hashpsd})
-    await newUser.save()
+    const newUser = new User({ email, password: hashpsd });
+    await newUser.save();
     res.json({
-        success:true,
-        message:"User Successfully Created"
-    })
-
-
-
-
-}catch(e){
-    console.log("server error ",e)
+      success: true,
+      message: "User Successfully Created",
+    });
+  } catch (e) {
+    console.log("server error ", e);
     res.json({
-        success:false,
-        message:"Server Error"
-    })
-}
-
-
-}
-
-exports.saveData = async function(req,res){
-
-
-
-  console.log("saving data ")
-  try{
-
-    
-     const product = await new ProductSchema(req.body)
-     await product.save()
-
-     res.json({
-      success:true,
-      message:"Added"
-     })
-
-  }catch(e){
-    res.json({
-      success:false,
-      error:e?.message
-    })
+      success: false,
+      message: "Server Error",
+    });
   }
+};
 
-  
+exports.saveData = async function (req, res) {
+  console.log("saving data ");
+  try {
+    const product = await new ProductSchema(req.body);
+    await product.save();
 
+    res.json({
+      success: true,
+      message: "Added",
+    });
+  } catch (e) {
+    res.json({
+      success: false,
+      error: e?.message,
+    });
+  }
+};
 
+exports.getAllProducts = async function (req, res) {
+  console.log("getting All products ");
+  try {
+    const products = await ProductSchema.find({});
+    if (!products) {
+      res.json({
+        success: false,
+        message: "No Products Found in DB",
+      });
+    }
 
-
-
-
-}
+    res.json({
+      success: true,
+      message: products,
+    });
+  } catch (e) {
+    res.json({
+      success: false,
+      error: e?.message,
+    });
+  }
+};
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,8 @@
-const userController = require('../controller/user')
-module.exports = function(app){
-    app.route('/login').post(userController.login)
-    app.route('/register').post(userController.register)
+const userController = require("../controller/user");
+module.exports = function (app) {
+  app.route("/login").post(userController.login);
+  app.route("/register").post(userController.register);
 
-    app.route('/saveData').post(userController.saveData)
-}
+  app.route("/saveData").post(userController.saveData);
+  app.route("/getAllProducts").get(userController.getAllProducts);
+};
